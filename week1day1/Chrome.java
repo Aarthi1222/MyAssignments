cb7a807a5bf494522e3f8c88cdcba34ec6366ecd
Aarthi1222 <traiotes25@gmail.com>
2025-05-01 16:30:32 +0530

    Add files via upload
    Day1

---
+++ week1day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1day1;
+
+public class Chrome {
+	public static void main(String[] args)
+	{
+		float map=91.0f;
+		System.out.println(map);
+		System.out.println("Google");
+		boolean result=true;
+		System.out.println(result);
+		int year=2008;
+		System.out.println("year:"+year);
+		char alphabet='c';
+		System.out.println(alphabet);
+		
+		
+	}
+	}
+
---
+++ week1day1/Hellotestleaf.java
@@ -0,0 +1,9 @@
+package week1day1;
+
+public class Hellotestleaf {
+public static void main (String[] args) {
+	System.out.println("welcome to testleaf");
+	
+}
+
+}
